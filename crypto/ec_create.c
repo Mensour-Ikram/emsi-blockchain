13a83d2246109e30a5dd196ec4d94401e6ed651e
mensourikram <mensourikram@gmail.com>
2021-04-11 13:27:51 +0000

    First commit

--- crypto/ec_create.c
+++ crypto/ec_create.c
@@ -1,11 +1,10 @@
-#include "hblk_crypto.h"
-
-
+#include<hblk_crypto.h>
 EC_KEY *ec_create(void)
 {
-	EC_KEY *k; 
+	EC_KEY *k;
 
-	k = EC_KEY_new_by_curve_name(EC_CURVE); 
+	k = EC_KEY_new_by_curve_name(EC_CURVE);
+
 	if (k == NULL)
 		return (NULL);
 	else if (!EC_KEY_generate_key(k))
