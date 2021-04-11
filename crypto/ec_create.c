27f3d79e854f01c7f7503898ba05f15fc759e30b
mensourikram <mensourikram@gmail.com>
2021-04-11 12:35:39 +0000

    Creating an EC_Key

---
+++ crypto/ec_create.c
@@ -0,0 +1,18 @@
+#include "hblk_crypto.h"
+
+
+EC_KEY *ec_create(void)
+{
+	EC_KEY *k; 
+
+	k = EC_KEY_new_by_curve_name(EC_CURVE); 
+	if (k == NULL)
+		return (NULL);
+	else if (!EC_KEY_generate_key(k))
+	{
+		EC_KEY_free(k);
+		return (NULL);
+	}
+	return (k);
+}
+
