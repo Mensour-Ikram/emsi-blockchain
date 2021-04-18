5a59cc162999e3a4043fb234595ec9fd950d1d34
mensourikram <mensourikram@gmail.com>
2021-04-18 15:49:30 +0000

    Fix

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -11,16 +11,18 @@
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
 				 sig_t *sig)
 {
-	unsigned int len;
+	uint32_t len = 0;
 
-	if (!key || !msg || !sig)
+	if (!key || !msg || !msglen)
 		return (NULL);
-	len = sig->len;
 
-	if (ECDSA_sign(0, msg, msglen, sig->sig, &len,
-				   (EC_KEY *)key) != 1)
+	memset(sig->sig, 0, sizeof(sig->sig));
+	if (!ECDSA_sign(0, msg, (int)msglen, sig->sig, &len, (EC_KEY *)key))
+	{
+		sig->len = 0;
 		return (NULL);
-	sig->len = len;
+	}
+	sig->len = (uint8_t)len;
 	return (sig->sig);
 }
 
