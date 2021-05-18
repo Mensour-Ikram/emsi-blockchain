bacd20b9d6783a4a3894e7e263266e9f2d2e982f
mensourikram <mensourikram@gmail.com>
2021-05-18 10:55:28 +0000

    Fix

--- blockchain/v0.2/block_is_valid.c
+++ blockchain/v0.2/block_is_valid.c
@@ -1,5 +1,13 @@
 #include "blockchain.h"
 
+/**
+*block_is valid - check if a block is valid
+* @block: pointer to the block that we want to verify
+* @prev_block: pointer to the prev block
+* Return: 1 if the block is invalid
+* 0 if the block is valid
+*/
+
 int block_is_valid(block_t const *block, block_t const *prev_block)
 {
 uint8_t hash_buffer[SHA256_DIGEST_LENGTH] = {0};
