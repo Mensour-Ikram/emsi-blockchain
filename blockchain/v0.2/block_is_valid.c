98ae0d16b333858c9497badb59056a0117afac9d
mensourikram <mensourikram@gmail.com>
2021-05-19 00:05:57 +0000

    last fix task0

--- blockchain/v0.2/block_is_valid.c
+++ blockchain/v0.2/block_is_valid.c
@@ -1,7 +1,7 @@
 #include "blockchain.h"
 
 /**
-*block_is valid - check if a block is valid
+* block_is_valid - check if a block is valid
 * @block: pointer to the block that we want to verify
 * @prev_block: pointer to the prev block
 * Return: 1 if the block is invalid
--- blockchain/v0.2/blockchain_difficulty.c
+++ blockchain/v0.2/blockchain_difficulty.c
@@ -11,16 +11,16 @@ return (0);
 if (l_block->info.index % DIFFICULTY_ADJUSTMENT_INTERVAL
 && l_block->info.index != 0){
 adj_block = llist_get_node_at(blockchain->chain,
-last_block->info.index + 1 - DIFFICULTY_ADJUSTMENT_INTERVAL)
-expected_elapsed_time = DIFFICULTY_ADJUSTEMENT_INTERVAL *
-BLOCK_GENERATION_INTERVAL;
+l_block->info.index + 1 - DIFFICULTY_ADJUSTMENT_INTERVAL);
+expected_elapsed_time = DIFFICULTY_ADJUSTEMENT_INTERVAL * BLOCK_GENERATION_INTERVAL;
 actual_elapsed_time = l_block->info.timestamp - adj_block->info.timestamp;
 if (actual_elapsed_time < expected_elapsed_time * 0.5)
 return (l_block->info.difficulty + 1);
-else if (actual_elapsed_time > 2 * expected elapsed_time)
+else if (actual_elapsed_time > 2 * expected_elapsed_time)
 return (l_block->info.difficulty - 1);
 else
 return (l_block->info.difficulty);
 }
+else
 return (l_block->info.difficulty);
 
